4f1be11c47d9090135cf01d13aad6cf045b12da7
nohandsome <1797695463@qq.com>
2022-03-14 12:15:04 +0800

    deal message from sever

--- version3/main2.py
+++ version3/main2.py
@@ -3,19 +3,34 @@ import threading
 from json import dumps
 import requests
 from requests import utils
-
-websocket.enableTrace(True)
+from json import loads
 
 
 # TO BE DONE
-# def deal_message():
-
+def deal_message(json_data):
+    data = loads(json_data)
+    if data['type'] == 'push':
+        print('(get push from sever ===> )' + 'Event:', data['body']['event'] + ' _CID:', data['body']['_CID'])
+    elif data['type'] == 'response':
+        if data['body']['code'] != 0:
+            print('(something wrong!) ===>' + data['body']['msg'])
+        else:
+            print('(get response from sever) ===>')
+            if 'contacts' in data['body']:
+                print('联系人:')
+                print(data['body']['contacts'])
+            elif 'messages' in data['body']:
+                print('消息' + data['body']['messages'])
+            else:
+                print('success!')
 
 
 def on_open(ws):
+    print('connect success!')
+
     def run(*args):
         while True:
-            cmd = input('input your command:')
+            cmd = input()
             body = {}
             for elem in ['sort', 'key', 'num', '_CID', 'lastMsg_MID', 'status', 'message']:
                 print('input {}:'.format(elem))
@@ -28,20 +43,26 @@ def on_open(ws):
     threading.Thread(target=run).start()
 
 
+def on_error(ws, error):
+    print(error)
+
+
 def on_message(ws, message):
     def run(*args):
-        print('(get) ===> ', message)
-        ws.close()
+        deal_message(message)
         print("Message received...")
 
     threading.Thread(target=run).start()
 
 
 def on_close(ws, close_status_code, close_msg):
+    if close_status_code or close_msg:
+        print('close status code:', close_status_code)
+        print('close msg:', close_msg)
     print(">>>>>>CLOSED")
 
 
-url_connect = ''
+url_connect = 'https://baidu.com/'
 play_load = {'username': '', 'password': ''}
 headers = {}
 r = requests.post(url_connect, data=dumps(play_load), headers=headers)
@@ -49,12 +70,15 @@ print('connecting to {}'.format(url_connect))
 if r.status_code != 200:
     print('登录失败，原因：{}'.format(r.reason))
     exit()
+else:
+    print('request success!')
 cookies_dic = requests.utils.dict_from_cookiejar(r.cookies)
 print(cookies_dic)
 cookie_str = ''
 for key in cookies_dic:
     cookie_str += key + '=' + cookies_dic[key] + ';'
 cookie_str = cookie_str.strip(';')
-wsapp = websocket.WebSocketApp("wss://api.bitfinex.com/ws/1",
-                               on_open=on_open, on_message=on_message, on_close=on_close, cookie=cookie_str)
+wsapp = websocket.WebSocketApp("ws://localhost:8765",
+                               on_open=on_open, on_message=on_message,
+                               on_close=on_close, on_error=on_error, cookie=cookie_str)
 wsapp.run_forever()
